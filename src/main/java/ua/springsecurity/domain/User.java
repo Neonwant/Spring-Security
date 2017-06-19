c0f314ec85b68cf57f5d1bd629f3a5bdedc7ff60
Vladyslav Glukhov <vladyslavglukhov@gmail.com>
2017-06-19 16:38:51 +0300

    add loadUserByUsername and resolve dependency

--- src/main/java/ua/springsecurity/domain/User.java
+++ src/main/java/ua/springsecurity/domain/User.java
@@ -1,53 +1,23 @@
 package ua.springsecurity.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
-import org.springframework.security.core.GrantedAuthority;
+import lombok.NoArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 
-import java.util.Collection;
 import java.util.List;
 
 @Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class User implements UserDetails {
-    private List<GrantedAuthority> authorities;
+    private List<Role> authorities;
     private String username;
     private String password;
     private boolean accountNonExpired;
     private boolean accountNonLocked;
+    private boolean credentialsNonExpired;
     private boolean enabled;
-
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        return null;
-    }
-
-    @Override
-    public String getPassword() {
-        return null;
-    }
-
-    @Override
-    public String getUsername() {
-        return null;
-    }
-
-    @Override
-    public boolean isAccountNonExpired() {
-        return false;
-    }
-
-    @Override
-    public boolean isAccountNonLocked() {
-        return false;
-    }
-
-    @Override
-    public boolean isCredentialsNonExpired() {
-        return false;
-    }
-
-    @Override
-    public boolean isEnabled() {
-        return false;
-    }
 }
--- src/main/java/ua/springsecurity/services/UserService.java
+++ src/main/java/ua/springsecurity/services/UserService.java
@@ -1,12 +1,23 @@
 package ua.springsecurity.services;
 
+import com.google.common.collect.ImmutableList;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import ua.springsecurity.domain.Role;
+import ua.springsecurity.domain.User;
 
 public class UserService implements UserDetailsService {
     @Override
-    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
-        return null;
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return User.builder()
+                .username("user")
+                .password("password")
+                .authorities(ImmutableList.of(Role.USER))
+                .accountNonExpired(true)
+                .accountNonLocked(true)
+                .credentialsNonExpired(true)
+                .enabled(true)
+                .build();
     }
 }
