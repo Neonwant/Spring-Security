1f02897e1b0fe663a62650a4b7bbc2b18a43e7b9
Vladyslav Glukhov <vladyslavglukhov@gmail.com>
2017-06-19 18:38:13 +0300

    refactoring

--- src/main/java/ua/springsecurity/services/UserService.java
+++ src/main/java/ua/springsecurity/services/UserService.java
@@ -5,12 +5,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 import ua.springsecurity.domain.Role;
 import ua.springsecurity.domain.User;
 import ua.springsecurity.persistence.UserDao;
 
 import javax.annotation.PostConstruct;
 
+@Service
 public class UserService implements UserDetailsService {
     @Autowired
     private UserDao userDao;
@@ -35,14 +37,5 @@ public class UserService implements UserDetailsService {
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         return userDao.findByUserName(username).orElseThrow(() ->
                 new UsernameNotFoundException("user" + username + " was not found!"));
-//        return User.builder()
-//                .username("user")
-//                .password("password")
-//                .authorities(ImmutableList.of(Role.USER))
-//                .accountNonExpired(true)
-//                .accountNonLocked(true)
-//                .credentialsNonExpired(true)
-//                .enabled(true)
-//                .build();
     }
 }
--- src/main/java/ua/springsecurity/web/SecurityConfig.java
+++ src/main/java/ua/springsecurity/web/SecurityConfig.java
@@ -1,11 +1,15 @@
 package ua.springsecurity.web;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import sun.security.util.Password;
 import ua.springsecurity.services.UserService;
 
 @Configuration
@@ -26,8 +30,14 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     private UserService userService;
 
+    @Bean
+    public PasswordEncoder bcrpyptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userService);
+        auth.userDetailsService(userService)
+                .passwordEncoder(bcrpyptPasswordEncoder());
     }
 }
