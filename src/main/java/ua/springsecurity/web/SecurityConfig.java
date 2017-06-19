0207658d12092ef71226d6dfc706849296d65fa7
Vladyslav Glukhov <vladyslavglukhov@gmail.com>
2017-06-19 16:14:57 +0300

    add User (Entity)

--- src/main/java/ua/springsecurity/web/SecurityConfig.java
+++ src/main/java/ua/springsecurity/web/SecurityConfig.java
@@ -24,6 +24,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService()
+        //auth.userDetailsService()
     }
 }
