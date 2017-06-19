9a909ef900f26c5cebe67b371426ff13d4d7211a
Vladyslav Glukhov <vladyslavglukhov@gmail.com>
2017-06-19 15:12:23 +0300

    create account with user role

--- src/main/java/ua/springsecurity/Application.java
+++ src/main/java/ua/springsecurity/Application.java
@@ -1,7 +1,9 @@
 package ua.springsecurity;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -11,5 +13,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class Application {
     public static void main(String[] args) throws Exception {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication().withUser("user").password("password").roles("USER");
     }
 }
