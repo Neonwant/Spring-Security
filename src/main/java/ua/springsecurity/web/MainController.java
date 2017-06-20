895c7ef9b30854e59ef27777dc934f5b231af565
Vladyslav Glukhov <vladyslavglukhov@gmail.com>
2017-06-20 17:05:38 +0300

    show roles in index.ftl

--- src/main/java/ua/springsecurity/web/MainController.java
+++ src/main/java/ua/springsecurity/web/MainController.java
@@ -1,16 +1,27 @@
 package ua.springsecurity.web;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import ua.springsecurity.domain.Role;
+import ua.springsecurity.domain.User;
+
+import java.security.Security;
+
+import static java.util.stream.Collectors.joining;
 
 @Controller
 public class MainController {
     @RequestMapping("/")
     public String getMainPage(Model model) {
-        model.addAttribute("username", "from model username");
-        model.addAttribute("roles", "from model roles");
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        User user = (User) authentication.getPrincipal();
+
+        model.addAttribute("username", user.getUsername());
+        model.addAttribute("roles", user.getAuthorities().stream().map(Role::getAuthority).collect(joining(",")));
         return "index";
     }
 
